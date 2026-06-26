ca6e7285089ba795431d7bd96320ac0c9129a544
mdmonimul95-crypto <mdmonimul95@gmail.com>
2026-06-26 13:00:25 +0600

    something update added

---
+++ src/app/categories/page.jsx
@@ -0,0 +1,11 @@
+
+import Catagories from "@/components/Catagories";
+
+export default function Home() {
+  return (
+        <div >
+     {/* <HeroBanner /> */}
+    <Catagories/>
+    </div>
+  );
+}
